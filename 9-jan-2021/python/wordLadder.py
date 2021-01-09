55c22b26720c566e82e3b94e36b9f2d968498cf8
rajibmitra <rajib.jolite@gmail.com>
2021-01-09 16:27:32 +0530

    basic sol

---
+++ 9-jan-2021/python/wordLadder.py
@@ -0,0 +1,7 @@
+# class Solution:
+#Once the method is ready, will then move to class
+def ladderLength(beginWord: str, endWord: str, wordList: list[str]) -> int:
+        print("Hello world")
+
+
+ladderLength("w","x",["w","x"])
