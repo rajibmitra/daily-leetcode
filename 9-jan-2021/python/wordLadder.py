80f578b3650357a70c4e1579e995489970d9cea7
rajibmitra <rajib.jolite@gmail.com>
2021-01-09 18:13:47 +0530

    python solution

--- 9-jan-2021/python/wordLadder.py
+++ 9-jan-2021/python/wordLadder.py
@@ -1,7 +1,25 @@
-# class Solution:
-#Once the method is ready, will then move to class
-def ladderLength(beginWord: str, endWord: str, wordList: list[str]) -> int:
-        print("Hello world")
+import collections
 
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
 
-ladderLength("w","x",["w","x"])
+        if beginWord in wordList:
+            wordList.remove(beginWord)
+        
+        if endWord not in wordList:
+            return 0
+
+        wordList.append(endWord)
+        q = collections.deque([[beginWord, 1]])
+        leng = len(beginWord)
+        
+        while q:
+            word, length = q.popleft()
+            if word == endWord:
+                return length
+            wordListCopy = wordList.copy()
+            for n in wordListCopy:
+                if sum(n[i]!=word[i] for i in range(leng))==1:
+                    wordList.remove(n)
+                    q.append([n,length+1])
+        return 0
